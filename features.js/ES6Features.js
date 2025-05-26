2c9fffdf045846396aa1ea09f79edd151983002c
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-05-26 23:21:58 +0100

    ES6 Features

--- features.js/ES6Features.js
+++ features.js/ES6Features.js
@@ -11,45 +11,60 @@
 // }
 
 // Pass By value and Pass by reference
-let name = "John";
-function person(val) {
-  console.log("Name", val);
-}
-person("Atikur");
+// let name = "John";
+// function person(val) {
+//   console.log("Name", val);
+// }
+// person("Atikur");
 
 // Pass By reference
-let array = [10, 20, 30, 40, 50];
-function add(array) {
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    array[i] = element * element;
-  }
-  console.log(array);
-}
-console.log(add(array));
+// let array = [10, 20, 30, 40, 50];
+// function add(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     array[i] = element * element;
+//   }
+//   console.log(array);
+// }
+// console.log(add(array));
 
 // var and let
-function main() {
-  var x = 10;
-  if (x === 10) {
-    const y = 20;
-    console.log(x + y);
-  }
-  console.log(y);
-}
-main();
+// function main() {
+//   var x = 10;
+//   if (x === 10) {
+//     const y = 20;
+//     console.log(x + y);
+//   }
+//   console.log(y);
+// }
+// main();
 
 //? Scope(Global,function,Block Scope)
-const firstName = "John";
-function add() {
-  let age = 20;
-  console.log(age);
-  function modify() {
-    let isMarried = false;
-    console.log(age + 20, isMarried);
-  }
-  isMarried = true;
-  console.log("His maretial status is", isMarried);
-  modify();
-}
-add();
+//const firstName = "John";
+// function scope
+// function add() {
+//   let age = 20;
+//   console.log(age);
+//   function modify() {
+//     let isMarried = false;
+//     console.log(age + 20, isMarried);
+//   }
+//   isMarried = true;
+//   console.log("His material status is", isMarried);
+//   modify();
+// }
+// add();
+
+// Block Scope
+// const isStudent = true;
+// function modifyStudent(ans){
+//     let courseAccess = true;
+//     if(ans){
+//          let supportSessionAccess = "available";
+//          console.log(courseAccess,supportSessionAccess);
+//     }
+// }
+// modifyStudent(isStudent);
+
+
+// Higher Order and Callback functions
